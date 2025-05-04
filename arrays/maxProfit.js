3e8e9ca7874f870de585fdb252c3478b474ad523
Temiloluwa <temiloluwaOgunti8@gmail.com>
2025-05-04 01:03:01 +0100

    Algo..

---
+++ arrays/maxProfit.js
@@ -0,0 +1,15 @@
+// Brute force
+var maxProfit = function (prices) {
+  let max = 0;
+  for (let i = prices.length - 1; i >= 0; i--) {
+    for (let j = i - 1; j >= 0; j--) {
+      max = Math.max(max, prices[i] - prices[j]);
+    }
+  }
+  return max;
+};
+
+// console.log(maxProfit([7, 1, 5, 3, 6, 4])); // Output: 5 (Buy on day 2 and sell on day 5)
+// console.log(maxProfit([7, 6, 4, 3, 1])); // Output: 0 (No transactions are done, i.e. max profit = 0)
+console.log(maxProfit([2, 4, 1])); // Output: 2 (Buy on day 1 and sell on day 2)
+console.log(maxProfit([1, 2])); // Output: 1 (Buy on day 1 and sell on day 2)
