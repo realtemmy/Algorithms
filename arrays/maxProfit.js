eb0ca829524a87d6e30012bf2ba7c111ed52263c
Temiloluwa <temiloluwaOgunti8@gmail.com>
2025-05-05 15:54:43 +0100

    Optimized max profit problem

--- arrays/maxProfit.js
+++ arrays/maxProfit.js
@@ -9,7 +9,22 @@ var maxProfit = function (prices) {
   return max;
 };
 
-// console.log(maxProfit([7, 1, 5, 3, 6, 4])); // Output: 5 (Buy on day 2 and sell on day 5)
-// console.log(maxProfit([7, 6, 4, 3, 1])); // Output: 0 (No transactions are done, i.e. max profit = 0)
-console.log(maxProfit([2, 4, 1])); // Output: 2 (Buy on day 1 and sell on day 2)
-console.log(maxProfit([1, 2])); // Output: 1 (Buy on day 1 and sell on day 2)
+// Optimized
+const maxProfit2 = function (prices) {
+  let minPrice = Infinity,
+    maxProfit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if(prices[i] < minPrice){
+      minPrice = prices[i]
+    }else{
+      maxProfit = Math.max(maxProfit, prices[i] - minPrice)
+    }
+  }
+
+  return maxProfit;
+};
+
+console.log(maxProfit2([7, 1, 5, 3, 6, 4])); // Output: 5 (Buy on day 2 and sell on day 5)
+console.log(maxProfit2([7, 6, 4, 3, 1])); // Output: 0 (No transactions are done, i.e. max profit = 0)
+console.log(maxProfit2([2, 4, 1])); // Output: 2 (Buy on day 1 and sell on day 2)
+console.log(maxProfit2([1, 2])); // Output: 1 (Buy on day 1 and sell on day 2)
